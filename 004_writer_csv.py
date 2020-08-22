fb863aa925ca7365b43c881450ad64513da66bc8
Salvador Vizcaino Martinez <svizcaino@pcolorada.com>
2020-08-22 13:05:04 -0500

    Added with random fake enter list into CSV

--- 004_writer_csv.py
+++ 004_writer_csv.py
@@ -1,10 +1,17 @@
 import csv
+import random
 
 
 # CONSTANT
 FILENAME = 'writing_csv.csv'
 COLUMN_NAMES = ['employee_id', 'entered_at', 'enter_for']
 MAX_NUMBER_LOGS = 100
+
+
+# FUNCTION
+def fake_enter_for():
+    enter_for_list = ['suppliement','reports','meeting','payment','bathroom','call','lunch']
+    return random.choice(enter_for_list)
 
 with open(FILENAME, mode='w',newline='') as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=COLUMN_NAMES)
@@ -16,6 +23,6 @@ with open(FILENAME, mode='w',newline='') as csv_file:
         writer.writerow({
             'employee_id': 1002, 
             'entered_at': 716253576253, 
-            'enter_for': 'LUNCH'
+            'enter_for': fake_enter_for()
         })
         counter += 1  # or counter = counter + 1
