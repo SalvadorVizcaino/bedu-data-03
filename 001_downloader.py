f5c95b313663815e8120edb1796c86830f5c4f64
Salvador Vizcaino Martinez <svizcaino@pcolorada.com>
2020-08-22 12:17:52 -0500

    Added function to add custom filename for images

--- 001_downloader.py
+++ 001_downloader.py
@@ -1,8 +1,12 @@
+from datetime import datetime
 import requests
+
 
 # CONSTANTS
 URL_BASE = 'https://api.github.com/'
 
+
+# FUNTIONS
 def get_github_user(username):
     url = f'{URL_BASE}users/{username}'
     response = requests.get(url)
@@ -14,11 +18,17 @@ def download_user_avatar(image_url):
     response = requests.get(image_url)
     if response.status_code == 200:
         response_content = response.content
-        filename = 'tmp/useravatar.png'
+        filename = f'tmp/{image_filename()}.png'
         with open(filename, 'wb') as image:
             image.write(response_content)
             return filename
     return None
+
+def image_filename():
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+    return timestamp
+
 
 username = input('Give the user name:\t')
 user = get_github_user(username)
